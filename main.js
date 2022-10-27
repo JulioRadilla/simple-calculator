e45534a746f158c9ca6a1ad4a4d9bdddd20b2e45
Julio Radilla <Itsdigitaljulio@gmail.com>
2022-10-26 21:56:31 -0500

    Finished JS and HTML Files for simple calculator

--- main.js
+++ main.js
@@ -12,15 +12,26 @@ document.querySelector('#zero').addEventListener('click',zero);
 document.querySelector('#one').addEventListener('click',one);
 document.querySelector('#two').addEventListener('click',two);
 document.querySelector('#three').addEventListener('click',three);
+document.querySelector('#four').addEventListener('click',four);
+document.querySelector('#five').addEventListener('click',five);
+document.querySelector('#six').addEventListener('click',six);
+document.querySelector('#seven').addEventListener('click',seven);
+document.querySelector('#eight').addEventListener('click',eight);
+document.querySelector('#nine').addEventListener('click',nine);
 //Event listener for operators
 document.querySelector('#plus').addEventListener('click',plus);
 document.querySelector('#minus').addEventListener('click',minus);
 document.querySelector('#multiply').addEventListener('click',multiply);
 document.querySelector('#divide').addEventListener('click',divide);
 document.querySelector('#equals').addEventListener('click',equals);
-// Functions for the event listener to do for each button and click zone
+document.querySelector('#clear').addEventListener('click',clear);
+// Functions for the event listener of numbers
 function zero(){
-    num1 = 0;
+    if(operator == ''){
+        num1 += '0';
+    } else {
+        num2 = num2 + '0';
+      }
 }
 
 function one(){
@@ -29,10 +40,72 @@ function one(){
     } else {
         num2 = num2 + '1';
       }
-    
-
 }
 
+function two(){
+    if(operator == ''){
+        num1 += '2';
+    } else {
+        num2 = num2 + '2';
+      }
+}
+
+function three(){
+    if(operator == ''){
+        num1 += '3';
+    } else {
+        num2 = num2 + '3';
+      }
+}
+
+function four(){
+    if(operator == ''){
+        num1 += '4';
+    } else {
+        num2 = num2 + '4';
+      }
+}
+
+function five(){
+    if(operator == ''){
+        num1 += '5';
+    } else {
+        num2 = num2 + '5';
+      }
+}
+
+function six(){
+    if(operator == ''){
+        num1 += '6';
+    } else {
+        num2 = num2 + '6';
+      }
+}
+
+function seven(){
+    if(operator == ''){
+        num1 += '7';
+    } else {
+        num2 = num2 + '7';
+      }
+}
+
+function eight(){
+    if(operator == ''){
+        num1 += '8';
+    } else {
+        num2 = num2 + '8';
+      }
+}
+
+function nine(){
+    if(operator == ''){
+        num1 += '9';
+    } else {
+        num2 = num2 + '9';
+      }
+}
+//Function for the event listeners of operators 
 function plus(){
     operator = '+';
 }
